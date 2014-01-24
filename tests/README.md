4417dd8359c808a5ccaeb8b2e41347a6418423bd
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2014-01-24 10:16:18 -0500

    Refactored testing, added valid cases

--- tests/README.md
+++
@@ -1,4 +0,0 @@
-xapi-proxy
-==========
-
-Protect LRS credentials by not exposing them to the browser at all.
--- tests/clean_test.js
+++ tests/clean_test.js
@@ -1,10 +1,22 @@
 var assert = require("assert"),
 	request = require("request");
 
+var validToken = '';
+
+var testData = {
+	'endpoint': 'https://lrs.adlnet.gov/xapi/',
+	'user': 'bogusUser',
+	'password': 'ButteredCatParadox',
+	'actor': {
+		'name': 'Steven Vergenz',
+		'mbox_sha1sum': 'd16681af53ecce8c2c650c5751575325af277cee'
+	}
+};
+
 describe('xAPI Proxy', function(){
 	describe('#storeLRSInfo', function()
 	{
-		it('should fail on empty POSTs', function(done)
+		it('should return 400 on empty POSTs', function(done)
 		{
 			request.post('http://localhost:3000/config', function(err,res,body){
 				if(err){
@@ -21,12 +33,13 @@ describe('xAPI Proxy', function(){
 			});
 		});
 
-		it('should fail on malformed POSTs', function(done)
+		it('should return 400 on malformed POSTs', function(done)
 		{
 			var testData = {
 				'endpoint': 'https://lrs.adlnet.gov/xapi/',
 				'user': 'bogusUser',
 				'password': 'ButteredCatParadox'
+				//missing actor
 			};
 
 			request.post('http://localhost:3000/config', {json:testData}, function(err,res,body){
@@ -36,6 +49,25 @@ describe('xAPI Proxy', function(){
 				}
 				try {
 					assert.strictEqual(res.statusCode, 400);
+					done();
+				}
+				catch(e){
+					done(e);
+				}
+			});
+		});
+
+		it('should return a token on valid POSTs', function(done)
+		{
+			request.post('http://localhost:3000/config', {json:testData}, function(err,res,body){
+				if(err){
+					done(err);
+					return;
+				}
+				try {
+					assert.strictEqual(res.statusCode, 200);
+					assert.ok(/^[A-Za-z0-9/+]{16}$/.test(body));
+					validToken = body;
 					done();
 				}
 				catch(e){
@@ -62,6 +94,7 @@ describe('xAPI Proxy', function(){
 				}
 			});
 		});
+
 		it('should return 404 when the token is not valid', function(done){
 			request.get('http://localhost:3000/config?xapi=this_key_should_not_be_found', function(err, res, body){
 				if(err){
@@ -70,6 +103,23 @@ describe('xAPI Proxy', function(){
 				}
 				try {
 					assert.strictEqual(res.statusCode, 404);
+					done();
+				}
+				catch(e){
+					done(e);
+				}
+			});
+		});
+
+		it('should return actor and expiration with a valid token', function(done){
+			request.get('http://localhost:3000/config?xapi='+encodeURIComponent(validToken), function(err, res, body){
+				if(err){
+					done(err);
+					return;
+				}
+				try {
+					assert.strictEqual(res.statusCode, 200);
+					assert.deepEqual(JSON.parse(body).actor, testData.actor);
 					done();
 				}
 				catch(e){
